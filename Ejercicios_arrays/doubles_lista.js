988cf615b9187522c64c0b4f8297ba5e8a25dab0
ElSanchecitos <diegomiguelsnchz@gmail.com>
2022-07-19 20:15:52 -0500

    ejercicio solucionado de doubles arrays

--- Ejercicios_arrays/doubles_lista.js
+++ Ejercicios_arrays/doubles_lista.js
@@ -1,1 +1,16 @@
-console.log("Hello world")
+//console.log("Hello world")
+
+function doubles_lista(array){
+
+    const doubles_result = []
+    var result 
+    for (ele in array){
+        //console.log(ele)
+        //console.log(array[ele])
+        result = array[ele] * 2 
+        doubles_result.push(result)
+    }
+    return doubles_result
+}
+
+console.log(doubles_lista([1,2,3,4,5]))
